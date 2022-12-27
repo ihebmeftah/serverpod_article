dbe29a28d18340143536b5a59b029f6ec7e0d0f1
iheb meftah <ihebmeftah666@gmail.com>
2022-12-28 00:45:07 +0100

    generate aricle class and table

---
+++ article_client/lib/src/protocol/article.dart
@@ -0,0 +1,57 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: implementation_imports
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+
+class Article extends _i1.SerializableEntity {
+  Article({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+    required this.isPrime,
+  });
+
+  factory Article.fromJson(
+    Map<String, dynamic> jsonSerialization,
+    _i1.SerializationManager serializationManager,
+  ) {
+    return Article(
+      id: serializationManager.deserialize<int?>(jsonSerialization['id']),
+      title:
+          serializationManager.deserialize<String>(jsonSerialization['title']),
+      content: serializationManager
+          .deserialize<String>(jsonSerialization['content']),
+      createdAt: serializationManager
+          .deserialize<DateTime>(jsonSerialization['createdAt']),
+      isPrime:
+          serializationManager.deserialize<bool>(jsonSerialization['isPrime']),
+    );
+  }
+
+  int? id;
+
+  String title;
+
+  String content;
+
+  DateTime createdAt;
+
+  bool isPrime;
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt,
+      'isPrime': isPrime,
+    };
+  }
+}
